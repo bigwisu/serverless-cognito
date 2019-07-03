0e79d28543b7ff9350d00a7570ca5e52dc9ce0cb
Wisu Suntoyo <wisu@devel.web.id>
2019-07-03 21:38:12 +0700

    fix yml

--- handler.js
+++ handler.js
@@ -21,3 +21,26 @@ module.exports.hiUsers = (event, context, callback) => {
 
   callback(null, response);
 };
+
+module.exports.presignup = (event, context, callback) => {
+
+  console.log("presignup exec");
+
+  // Confirm the user
+  event.response.autoConfirmUser = true;
+
+  // Set the email as verified if it is in the request
+  if (event.request.userAttributes.hasOwnProperty("email")) {
+      event.response.autoVerifyEmail = true;
+      console.log("Email Address",event.request.userAttributes.email);
+  }
+
+  // Set the phone number as verified if it is in the request
+  if (event.request.userAttributes.hasOwnProperty("phone_number")) {
+      event.response.autoVerifyPhone = true;
+      console.log("Phone Number",event.request.userAttributes.phone_number);
+  }
+
+  // Return to Amazon Cognito
+  callback(null, event);
+};
